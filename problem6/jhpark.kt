c41904c69287cbf31465a05923bb3ba2bc2c7647
dry-p <quebostina@gmail.com>
2017-04-14 01:02:41 +0900

    Create jhpark.kt

---
+++ problem6/jhpark.kt
@@ -0,0 +1,15 @@
+fun main(args: Array<String>) {
+    fun guess(i: Int, range:IntRange) {
+        fun check(range: IntRange) {
+            val c = (range.last + range.first) / 2
+            print(c.toString() + if (c == i) "!" else " ")
+
+            if (c > i) check(range.first..c)
+            else if (c < i) check(c..range.last)
+        }
+        check(range)
+    }
+    guess(readLine()?.toInt()!!, 1..100)
+
+    print((1..100).toList()[1])
+}
