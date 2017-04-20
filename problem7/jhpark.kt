625bb7220f74db0bf81ecffd10e8853a1ced01b1
dry-p <quebostina@gmail.com>
2017-04-21 08:47:47 +0900

    Update jhpark.kt

--- problem7/jhpark.kt
+++ problem7/jhpark.kt
@@ -2,7 +2,7 @@ import java.util.concurrent.LinkedBlockingQueue
 
 fun Josephus(n: Int, k:Int, t:Int): Int {
     val q = LinkedBlockingQueue<Int>(List(n, { it + 1 }))
-    do (0..k - 2).forEach { q.put(q.poll()) } while (q.poll() != t)
+    do { (0..k - 2).forEach { q.put(q.poll()) } } while (q.poll() != t)
     return n - q.size
 }
 
